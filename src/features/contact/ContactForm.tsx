3e26ad873137c5b83ddbb40c3ea654f45b647dce
Ngoc Anh <ngocanh030110@gmail.com>
2025-09-14 12:17:45 +0700

    test workflow

--- src/features/contact/ContactForm.tsx
+++ src/features/contact/ContactForm.tsx
@@ -25,10 +25,7 @@ function ContactForm() {
 		handleSubmit,
 		formState: { errors }
 	} = useForm<FormData>({ resolver: yupResolver(formSchema) })
-
-    console.log(`service_id ::: `,import.meta.env.VITE_EMAILJS_SERVICE_ID)
-    console.log(`template_id ::: `,import.meta.env.VITE_EMAILJS_TEMPLATE_ID)
-
+    
 	const sendEmail = handleSubmit(async (data: FormData) => {
 		setLoading(true)
 		setSuccess(null)
